53319fb0ee3dc4574f7290c29306dae032d6f510
Jerry Allan Akshay <allanakshay@outlook.com>
2020-04-25 14:55:12 +0530

    Added the basic fragments for the app

--- LocalGrocer/app/src/main/java/com/example/localgrocer/ConsumerActivity.java
+++ LocalGrocer/app/src/main/java/com/example/localgrocer/ConsumerActivity.java
@@ -1,13 +1,21 @@
 package com.example.localgrocer;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
+import com.example.localgrocer.ui.Consumer.ConsumerFragment;
+import com.example.localgrocer.ui.MyProfile.MyProfileFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,9 +24,11 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class ConsumerActivity extends AppCompatActivity {
+public class ConsumerActivity extends AppCompatActivity  {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private  DrawerLayout drawer;
+    private Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,7 @@ public class ConsumerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_consumer);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        activity = this;
         //FloatingActionButton fab = findViewById(R.id.fab);
         /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,17 +45,59 @@ public class ConsumerActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        drawer = findViewById(R.id.drawer_layout);
+        final NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.bringToFront();
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_consumer_fragment, R.id.nav_requestshop_fragment, R.id.nav_manage_shop_fragment, R.id.nav_my_profile_fragment )
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        final NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                menuItem.setChecked(true);
+                drawer.closeDrawers();
+
+                switch (id) {
+                    case R.id.nav_my_profile:
+                        /*MyProfileFragment myProfileFragment = new MyProfileFragment();
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        fragmentManager.beginTransaction().replace(R.id.host_frame, myProfileFragment, myProfileFragment.getTag()).commit();
+                        setTitle(menuItem.getTitle());*/
+                        navController.navigate(R.id.nav_my_profile_fragment);
+                        break;
+                    case R.id.nav_consumer:
+                        navController.navigate(R.id.nav_consumer_fragment);
+                        break;
+                    case R.id.nav_requestshop:
+                        navController.navigate(R.id.nav_requestshop_fragment);
+                        break;
+                    case R.id.nav_manageshop:
+                        navController.navigate(R.id.nav_manage_shop_fragment);
+                        break;
+                    case R.id.nav_contact:
+                        menuItem.setChecked(false);
+                        break;
+                    case R.id.nav_log_out:
+                        menuItem.setChecked(false);
+                        break;
+                }
+
+                drawer.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
+
+        //navigationView.getMenu().findItem(R.id.nav_requestshop).setVisible(false);
+
     }
 
     @Override
@@ -60,4 +113,22 @@ public class ConsumerActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+   /* @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        menuItem.setChecked(true);
+        drawer.closeDrawers();
+
+        switch (id) {
+            case R.id.nav_my_profile:
+
+                MyProfileFragment myProfileFragment = new MyProfileFragment();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.nav_my_profile_fragment, myProfileFragment, myProfileFragment.getTag()).commit();
+                drawer.closeDrawer(GravityCompat.START);
+        }
+        return true;
+    }*/
 }
---
+++ LocalGrocer/app/src/main/java/com/example/localgrocer/ui/MyProfile/MyProfileFragment.java
@@ -0,0 +1,27 @@
+package com.example.localgrocer.ui.MyProfile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.localgrocer.R;
+
+public class MyProfileFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_myprofile, container, false);
+
+        return root;
+    }
+}
